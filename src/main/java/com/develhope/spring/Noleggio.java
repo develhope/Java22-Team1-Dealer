cf2e836955ee8f4590fefe7a29c5772acf4c00ee
Basile Bartolomeo <basilebartolomeo01@gmail.com>
2024-04-23 13:03:35 +0200

    added column nullability and names

--- src/main/java/com/develhope/spring/Noleggio.java
+++ src/main/java/com/develhope/spring/Noleggio.java
@@ -8,95 +8,43 @@ package com.develhope.spring;
 //Flag pagato
 //Veicolo noleggiato
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.LocalDate;
 
+@Data
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Noleggio {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, name = "Inizio noleggio")
     private LocalDate dataInizio;
+    @Column(nullable = false, name = "Fine noleggio")
     private LocalDate dataFine;
-    private double costoGiornaliero;
-    private double costoTotale;
-    private boolean pagato;
+    @Column(nullable = false, name = "Costo giornaliero")
+    private Double costoGiornaliero;
+    @Column(nullable = false, name = "Costo totale")
+    private Double costoTotale;
+    @Column(nullable = false, name = "Pagato")
+    private Boolean pagato;
+    @Column(nullable = false, name = "Veicolo")
     private Veicolo veicoloNoleggiato; //da unire con la sua classe
+    @Column(nullable = false, name = "Cliente")
     private Cliente cliente; //da unire con la sua classe
 
-    public Noleggio(LocalDate dataInizio, LocalDate dataFine, double costoGiornaliero, Veicolo veicoloNoleggiato, Cliente cliente) {
-        this.dataInizio = dataInizio;
-        this.dataFine = dataFine;
-        this.costoGiornaliero = costoGiornaliero;
-        this.veicoloNoleggiato = veicoloNoleggiato;
-        this.cliente = cliente;
-        this.calcolaCostoTotale();
-    }
 
     private void calcolaCostoTotale() {
         long giorni = java.time.temporal.ChronoUnit.DAYS.between(dataInizio, dataFine);
         this.costoTotale = giorni * costoGiornaliero;
     }
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDate getDataInizio() {
-        return dataInizio;
-    }
-
-    public void setDataInizio(LocalDate dataInizio) {
-        this.dataInizio = dataInizio;
-    }
-
-    public LocalDate getDataFine() {
-        return dataFine;
-    }
-
-    public void setDataFine(LocalDate dataFine) {
-        this.dataFine = dataFine;
-    }
-
-    public double getCostoGiornaliero() {
-        return costoGiornaliero;
-    }
-
-    public void setCostoGiornaliero(double costoGiornaliero) {
-        this.costoGiornaliero = costoGiornaliero;
-    }
-
-    public double getCostoTotale() {
-        return costoTotale;
-    }
-
-    public void setCostoTotale(double costoTotale) {
-        this.costoTotale = costoTotale;
-    }
-
-    public boolean isPagato() {
-        return pagato;
-    }
-
-    public void setPagato(boolean pagato) {
-        this.pagato = pagato;
-    }
-
-    public Veicolo getVeicoloNoleggiato() {
-        return veicoloNoleggiato;
-    }
-
-    public void setVeicoloNoleggiato(Veicolo veicoloNoleggiato) {
-        this.veicoloNoleggiato = veicoloNoleggiato;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
 }
