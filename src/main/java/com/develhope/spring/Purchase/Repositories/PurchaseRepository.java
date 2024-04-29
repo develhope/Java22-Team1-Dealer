a51e6c3c5a2ef165460e27063d810b011b2ea368
Cristian Tomulescu <150183775+crislynch@users.noreply.github.com>
2024-04-29 11:31:27 +0200

    fix import e cartelle

---
+++ src/main/java/com/develhope/spring/Purchase/Repositories/PurchaseRepository.java
@@ -0,0 +1,8 @@
+package com.develhope.spring.Purchase.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
+}
