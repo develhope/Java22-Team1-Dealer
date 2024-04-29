f4f30126036ae13227b5d79d3c0fe4ec5de02c3e
Cristian Tomulescu <150183775+crislynch@users.noreply.github.com>
2024-04-29 11:32:52 +0200

    fix import e cartelle

--- src/main/java/com/develhope/spring/purchase/PurchaseController.java
+++
@@ -1,7 +0,0 @@
-package com.develhope.spring.purchase;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class PurchaseController {
-}
--- src/main/java/com/develhope/spring/purchase/PurchaseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.develhope.spring.purchase;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
-}
