70b4a4ddb2d22e53708227ad69227302fc3d13ca
Basile Bartolomeo <basilebartolomeo01@gmail.com>
2024-05-01 20:30:32 +0200

    changed generated value in orderEntity

--- src/main/java/com/develhope/spring/order/Entities/OrderEntity.java
+++ src/main/java/com/develhope/spring/order/Entities/OrderEntity.java
@@ -16,7 +16,7 @@ import java.util.List;
 @NoArgsConstructor
 public class OrderEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
     @Column(nullable = false, name = "deposit")
     private int deposit;
