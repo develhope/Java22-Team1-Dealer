57776f3d66dc5d7be37ea12ed15960655b8283ab
cla-gambino <154328607+cla-gambino@users.noreply.github.com>
2024-05-01 12:01:24 +0200

    getAllCars function

--- src/main/java/com/develhope/spring/Vehicles/Entities/DTO/VehicleModel.java
+++ src/main/java/com/develhope/spring/Vehicles/Entities/DTO/VehicleModel.java
@@ -28,7 +28,9 @@ public class VehicleModel {
     private VehicleStatus vehicleStatus;
     private VehicleType vehicleType;
 
-    public VehicleModel(String brand, String model, Integer displacement, String color, Integer power, String transmission, Integer registrationYear, String powerSupply, BigDecimal price, BigDecimal discount, String accessories, Boolean isNew, VehicleStatus vehicleStatus, VehicleType vehicleType) {
+    public VehicleModel(String brand, String model, Integer displacement, String color, Integer power, String transmission,
+                        Integer registrationYear, String powerSupply, BigDecimal price, BigDecimal discount, String accessories,
+                        Boolean isNew, VehicleStatus vehicleStatus, VehicleType vehicleType) {
         this.brand = brand;
         this.model = model;
         this.displacement = displacement;
@@ -46,29 +48,29 @@ public class VehicleModel {
     }
 
     public static VehicleModel entityToModel(VehicleEntity vehicleEntity) {
-        return new VehicleModel(vehicleEntity.getVehicleId(), vehicleEntity.getBrand(), vehicleEntity.getModel(), vehicleEntity.getDisplacement(), vehicleEntity.getColor(),
-                vehicleEntity.getPower(), vehicleEntity.getTransmission(), vehicleEntity.getRegistrationYear(),
-                vehicleEntity.getPowerSupply(), vehicleEntity.getPrice(), vehicleEntity.getDiscount(), vehicleEntity.getAccessories(),
-                vehicleEntity.getIsNew(), vehicleEntity.getVehicleStatus(), vehicleEntity.getVehicleType());
+        return new VehicleModel(vehicleEntity.getVehicleId(), vehicleEntity.getBrand(), vehicleEntity.getModel(),
+                vehicleEntity.getDisplacement(), vehicleEntity.getColor(), vehicleEntity.getPower(), vehicleEntity.getTransmission(),
+                vehicleEntity.getRegistrationYear(), vehicleEntity.getPowerSupply(), vehicleEntity.getPrice(), vehicleEntity.getDiscount(),
+                vehicleEntity.getAccessories(), vehicleEntity.getIsNew(), vehicleEntity.getVehicleStatus(), vehicleEntity.getVehicleType());
     }
 
     public static VehicleModel DTOtoModel(VehicleDTO vehicleDTO) {
-        return new VehicleModel(vehicleDTO.getVehicleId(), vehicleDTO.getBrand(), vehicleDTO.getModel(), vehicleDTO.getDisplacement(), vehicleDTO.getColor(),
-                vehicleDTO.getPower(), vehicleDTO.getTransmission(), vehicleDTO.getRegistrationYear(),
+        return new VehicleModel(vehicleDTO.getVehicleId(), vehicleDTO.getBrand(), vehicleDTO.getModel(), vehicleDTO.getDisplacement(),
+                vehicleDTO.getColor(), vehicleDTO.getPower(), vehicleDTO.getTransmission(), vehicleDTO.getRegistrationYear(),
                 vehicleDTO.getPowerSupply(), vehicleDTO.getPrice(), vehicleDTO.getDiscount(), vehicleDTO.getAccessories(),
                 vehicleDTO.getIsNew(), vehicleDTO.getVehicleStatus(), vehicleDTO.getVehicleType());
     }
 
     public static VehicleEntity modelToEntity(VehicleModel vehicleModel) {
-        return new VehicleEntity(vehicleModel.getVehicleId(), vehicleModel.getBrand(), vehicleModel.getModel(), vehicleModel.getDisplacement(), vehicleModel.getColor(),
-                vehicleModel.getPower(), vehicleModel.getTransmission(), vehicleModel.getRegistrationYear(),
+        return new VehicleEntity(vehicleModel.getVehicleId(), vehicleModel.getBrand(), vehicleModel.getModel(), vehicleModel.getDisplacement(),
+                vehicleModel.getColor(), vehicleModel.getPower(), vehicleModel.getTransmission(), vehicleModel.getRegistrationYear(),
                 vehicleModel.getPowerSupply(), vehicleModel.getPrice(), vehicleModel.getDiscount(), vehicleModel.getAccessories(),
                 vehicleModel.getIsNew(), vehicleModel.getVehicleStatus(), vehicleModel.getVehicleType());
     }
 
     public static VehicleDTO modelToDTO(VehicleModel vehicleModel) {
-        return new VehicleDTO(vehicleModel.getVehicleId(), vehicleModel.getBrand(), vehicleModel.getModel(), vehicleModel.getDisplacement(), vehicleModel.getColor(),
-                vehicleModel.getPower(), vehicleModel.getTransmission(), vehicleModel.getRegistrationYear(),
+        return new VehicleDTO(vehicleModel.getVehicleId(), vehicleModel.getBrand(), vehicleModel.getModel(), vehicleModel.getDisplacement(),
+                vehicleModel.getColor(), vehicleModel.getPower(), vehicleModel.getTransmission(), vehicleModel.getRegistrationYear(),
                 vehicleModel.getPowerSupply(), vehicleModel.getPrice(), vehicleModel.getDiscount(), vehicleModel.getAccessories(),
                 vehicleModel.getIsNew(), vehicleModel.getVehicleStatus(), vehicleModel.getVehicleType());
     }
--- src/main/java/com/develhope/spring/Vehicles/Services/VehicleService.java
+++ src/main/java/com/develhope/spring/Vehicles/Services/VehicleService.java
@@ -12,7 +12,10 @@ import com.develhope.spring.Vehicles.Response.VehicleResponse;
 import io.vavr.control.Either;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class VehicleService {
 
@@ -23,13 +26,14 @@ public class VehicleService {
     UserRepository userRepository;
 
     public Either<VehicleResponse, VehicleDTO> createVehicle(Long id, VehicleRequest vehicleRequest) {
-        Optional<User> user = userRepository.findById(id);
-        if (user.isPresent()) {
-            if (user.get().getUserType() == UserTypes.ADMIN) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            if (userOptional.get().getUserType() == UserTypes.ADMIN) {
                 VehicleModel vehicleModel = new VehicleModel(vehicleRequest.getBrand(), vehicleRequest.getModel(),
                         vehicleRequest.getDisplacement(), vehicleRequest.getColor(),
-                        vehicleRequest.getPower(), vehicleRequest.getTransmission(), vehicleRequest.getRegistrationYear(), vehicleRequest.getPowerSupply(),
-                        vehicleRequest.getPrice(), vehicleRequest.getDiscount(), vehicleRequest.getAccessories(), vehicleRequest.getIsNew(), vehicleRequest.getVehicleStatus(),
+                        vehicleRequest.getPower(), vehicleRequest.getTransmission(), vehicleRequest.getRegistrationYear(),
+                        vehicleRequest.getPowerSupply(), vehicleRequest.getPrice(), vehicleRequest.getDiscount(),
+                        vehicleRequest.getAccessories(), vehicleRequest.getIsNew(), vehicleRequest.getVehicleStatus(),
                         vehicleRequest.getVehicleType());
                 VehicleEntity vehicleEntity = VehicleModel.modelToEntity(vehicleModel);
                 VehicleEntity result = vehicleRepository.save(vehicleEntity);
@@ -44,11 +48,11 @@ public class VehicleService {
     }
 
     public Either<VehicleResponse, VehicleDTO> getSingleVehicle(Long userId, Long vehicleId) {
-        Optional<User> user = userRepository.findById(userId);
-        if (user.isEmpty()) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isEmpty()) {
             return Either.left(new VehicleResponse(404, "user with id" + userId + "not found"));
         }
-        if (user.get().getUserType() != UserTypes.BUYER && user.get().getUserType() != UserTypes.SELLER) {
+        if (userOptional.get().getUserType() != UserTypes.BUYER && userOptional.get().getUserType() != UserTypes.SELLER) {
             return Either.left(new VehicleResponse(400, "only buyer and seller can get the vehicle"));
         }
         Optional<VehicleEntity> vehicleEntity = vehicleRepository.findById(vehicleId);
@@ -57,5 +61,26 @@ public class VehicleService {
         }
         VehicleModel vehicleModel = VehicleModel.entityToModel(vehicleEntity.get());
         return Either.right(VehicleModel.modelToDTO(vehicleModel));
+    }
+
+    public Either<VehicleResponse, List<VehicleDTO>> getAllCars(Long userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isEmpty()) {
+            return Either.left(new VehicleResponse(404, "user with id" + userId + "not found"));
+        }
+
+        List<VehicleEntity> vehicleEntity = vehicleRepository.findAll();
+        if (vehicleEntity.isEmpty()) {
+            return Either.left(new VehicleResponse(404, "no cars list found"));
+        }
+        List<VehicleModel> vehicleModels = vehicleEntity.stream()
+                .map(VehicleModel::entityToModel)
+                .toList();
+
+        List<VehicleDTO> vehicleDTOs = vehicleModels.stream()
+                .map(VehicleModel::modelToDTO)
+                .collect(Collectors.toList());
+
+        return Either.right(vehicleDTOs);
     }
 }
