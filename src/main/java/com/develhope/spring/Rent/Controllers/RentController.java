f62643c30bb26d9cd7cb8bbf7d210f3c1355b716
Cristian Tomulescu <150183775+crislynch@users.noreply.github.com>
2024-05-01 20:59:16 +0200

    aggiunta documentazione nel controller

--- src/main/java/com/develhope/spring/Rent/Controllers/RentController.java
+++ src/main/java/com/develhope/spring/Rent/Controllers/RentController.java
@@ -4,6 +4,11 @@ import com.develhope.spring.Rent.Entities.DTO.RentDTO;
 import com.develhope.spring.Rent.Entities.RentEntity;
 import com.develhope.spring.Rent.Response.RentResponse;
 import com.develhope.spring.Rent.Services.RentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.vavr.control.Either;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,6 +24,24 @@ public class RentController {
     @Autowired
     private RentService rentService;
 
+    @Operation(summary = "Create a rent with required data")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Successfully created rent",
+                            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RentDTO.class))}
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Unable to create rent"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "User with id {userId} not found"
+                    )
+            }
+    )
     @PostMapping("/create/{userId}")
     public ResponseEntity<?> createRent(@PathVariable Long userId, @RequestBody RentDTO rentDTO) {
         Either<RentResponse, RentDTO> result = rentService.createRent(userId, rentDTO);
@@ -28,6 +51,16 @@ public class RentController {
         return ResponseEntity.status(HttpStatus.CREATED).body(result.get());
     }
 
+    @Operation(summary = "Get a list of rents by user id")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully retrieved list of rents",
+                            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RentDTO.class))}
+                    )
+            }
+    )
     @GetMapping("/list/{userId}")
     public ResponseEntity<?> rentList(@PathVariable Long userId) {
         Either<RentResponse, List<RentDTO>> result = rentService.getRentsByUserId(userId);
@@ -37,6 +70,20 @@ public class RentController {
         return ResponseEntity.ok(result.get());
     }
 
+    @Operation(summary = "Get a rent by user id and rent id")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Rent successfully found",
+                            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RentDTO.class))}
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Rent with id {rentId} not found for user with id {userId}"
+                    )
+            }
+    )
     @GetMapping("/search/{userId}/{rentId}")
     public ResponseEntity<?> getRentById(@PathVariable Long userId, @PathVariable Long rentId) {
         Either<RentResponse, RentDTO> result = rentService.getRentById(userId, rentId);
@@ -46,6 +93,28 @@ public class RentController {
         return ResponseEntity.ok(result.get());
     }
 
+    @Operation(summary = "Update rent by id")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Updated rent with id: {rentId}",
+                            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RentDTO.class))}
+                    ),
+                    @ApiResponse(
+                            responseCode ="403",
+                            description = "This rent does not belong to the specified user with id: {userId}"
+                    ),
+                    @ApiResponse(
+                            responseCode ="404",
+                            description = "No rent found with id: {rentId}"
+                    ),
+                    @ApiResponse(
+                            responseCode ="404",
+                            description = "User with id: {userId}, is not found"
+                    )
+            }
+    )
     @PutMapping("/update/{userId}/{rentId}")
     public ResponseEntity<?> updateRentDates(@PathVariable Long userId, @PathVariable Long rentId, @RequestBody RentDTO rentDTO) {
         Either<RentResponse, RentDTO> result = rentService.updateRentDates(userId, rentId, rentDTO);
@@ -55,6 +124,19 @@ public class RentController {
         return ResponseEntity.ok(result.get());
     }
 
+    @Operation(summary = "Delete rent by id")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Rent deleted successfully"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "No rent found with id: {rentId} for user with id: {userId}"
+                    )
+            }
+    )
     @DeleteMapping("/remove/{userId}/{rentId}")
     public ResponseEntity<?> deleteRent(@PathVariable Long userId, @PathVariable Long rentId) {
         Either<RentResponse, Boolean> result = rentService.deleteRent(userId, rentId);
