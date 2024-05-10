f6692e1d93929324a3db9560b96ccbe2b2858506
Cristian Tomulescu <150183775+crislynch@users.noreply.github.com>
2024-05-10 14:25:23 +0200

    merge into develop of rentv10

--- src/main/java/com/develhope/spring/Rent/Controllers/RentController.java
+++ src/main/java/com/develhope/spring/Rent/Controllers/RentController.java
@@ -15,10 +15,7 @@ import io.vavr.control.Either;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
 import java.util.List;
 
 @RestController
