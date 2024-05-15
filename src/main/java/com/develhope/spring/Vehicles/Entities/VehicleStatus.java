98ab7096f00c2670a47d278e7036c6835afdbb0c
Cristian Tomulescu <150183775+crislynch@users.noreply.github.com>
2024-05-16 01:10:42 +0200

    Added RENTED and SOLD statuses to VehicleStatus enum, including conversion logic

--- src/main/java/com/develhope/spring/Vehicles/Entities/VehicleStatus.java
+++ src/main/java/com/develhope/spring/Vehicles/Entities/VehicleStatus.java
@@ -4,12 +4,16 @@ public enum VehicleStatus {
 
     RENTABLE,
     PURCHASABLE,
+    RENTED,
+    SOLD,
     NOT_AVAILABLE;
 
     public static VehicleStatus convertFromString(String status) {
         return switch (status.toLowerCase()) {
             case "rentable" -> VehicleStatus.RENTABLE;
             case "purchasable" -> VehicleStatus.PURCHASABLE;
+            case "rented" -> VehicleStatus.RENTED;
+            case "sold" -> VehicleStatus.SOLD;
             default -> VehicleStatus.NOT_AVAILABLE;
         };
     }
